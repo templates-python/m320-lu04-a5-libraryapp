b331ea772f649a9bcaad684babb9624e99084c70
Rene Probst <rene.probst@bzz.ch>
2023-08-22 17:49:15 +0200

    String customized for regex

--- test_main.py
+++ test_main.py
@@ -13,20 +13,7 @@ def test_main_part2(capsys):
     output = captured.out
 
     # Der erwartete reguläre Ausdruck
-    expected_pattern = r'''Bibliotheks-Anwendung\n=====================\n
-                           \n
-                           Kunden:\n
-                           Kunde: Moritz\n
-                           Kunde: Ursula\n
-                           ---\n
-                           Inventar:\n
-                           ISBN : 3-345-678 / Titel: Ich bin dann mal weg  / Ablage : [a-zA-Z0-9]{1,4}\n
-                           ISBN : 6-444-856 / Titel: im Westen nichts neues  / Ablage : [a-zA-Z0-9]{1,4}\n
-                           ISBN : 3-3345-678-X / Titel: Das Omen  / Ablage : [a-zA-Z0-9]{1,4}\n
-                           ISBN : 3-4321-334 / Titel: Harry Potter, die neue Welt  / Ablage : [a-zA-Z0-9]{1,4}\n
-                           ISBN : 3-2123-554 / Titel: die schönsten Zugreisen  / Ablage : [a-zA-Z0-9]{1,4}\n
-                           ---\n'''
-
+    expected_pattern = r'''Bibliotheks-Anwendung\n=====================\n\nKunden:\nKunde: Moritz\nKunde: Ursula\n---\nInventar:\nISBN : 3-345-678 / Titel: Ich bin dann mal weg  / Ablage : [A-Z][1-9]|10\nISBN : 6-444-856 / Titel: im Westen nichts neues  / Ablage : [A-Z][1-9]|10\nISBN : 3-3345-678-X / Titel: Das Omen  / Ablage : [A-Z][1-9]|10\nISBN : 3-4321-334 / Titel: Harry Potter, die neue Welt  / Ablage : [A-Z][1-9]|10\nISBN : 3-2123-554 / Titel: die schönsten Zugreisen  / Ablage : [A-Z][1-9]|10\n---\n'''
 
     # Überprüfung der Ausgabe mit dem regulären Ausdruck
     assert re.match(expected_pattern, output), f"Die Ausgabe entspricht nicht dem erwarteten Muster:\n {output}"
