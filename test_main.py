b6bfba06f3d80357de7e96f635e6c395af1c357a
Rene Probst <rene.probst@bzz.ch>
2023-08-25 08:37:31 +0200

    part 3 final commit

--- test_main.py
+++ test_main.py
@@ -17,24 +17,3 @@ def test_main_part2(capsys):
 
     # Überprüfung der Ausgabe mit dem regulären Ausdruck
     assert re.match(expected_pattern, output), f"Die Ausgabe entspricht nicht dem erwarteten Muster:\n {output}"
-
-def test_main_part3(capsys):
-    main()
-    captured = capsys.readouterr()
-    output = captured.out
-    # den schon getesteten Teil aus dem output löschen (Teilstring)
-    index = output.index('---')
-    index = output.index('---', index+1)
-    compared_string = output[index + len('---'):]
-
-    # Der erwartete reguläre Ausdruck
-    expected_pattern = r'''\nUrsula hat das Buch 'Das Omen' erhalten.\nMoritz hat das Buch 'Ich bin dann mal weg' erhalten.\nInventar:\nISBN: 6 - 444 - 856 / Titel: im Westen nichts neues / Ablage: [A-Z][1-9]|10\nISBN: 3 - 4321 - 334 / Titel: Harry Potter, die neue Welt / Ablage: [A-Z][1-9]|10\nISBN: 3 - 2123 - 554 / Titel: die schönsten Zugreisen / Ablage: [A-Z][1-9]|10\n---'''
-
-    #INLINE TEST
-    print(f'\nPATTERN:\n{expected_pattern}')
-    print('***')
-    print(f'\nOUTPUT :\n{compared_string}')
-    #INLINE TEST
-    # Überprüfung der Ausgabe mit dem regulären Ausdruck
-    assert re.match(expected_pattern, compared_string), f"Die Ausgabe entspricht nicht dem erwarteten Muster:\n {compared_string}"
-
